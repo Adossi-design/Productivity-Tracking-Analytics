7532e56533d643bff156d39fd045f9e554e3545f
Adossi Fred William <f.adossi@alustudent.com>
2026-04-29 14:25:11 +0200

    Create Project model for organizing work categories with Firestore document mapping

--- lib/models/project.dart
+++ lib/models/project.dart
@@ -1,29 +1,19 @@
-import 'dart:convert';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
+/// Project model with Firestore serialization
 class Project {
   final String id;
   final String name;
 
-  Project({
-    required this.id,
-    required this.name,
-  });
+  Project({required this.id, required this.name});
 
-  Map<String, dynamic> toMap() {
-    return {
-      'id': id,
-      'name': name,
-    };
+  Map<String, dynamic> toMap() => {'id': id, 'name': name};
+
+  factory Project.fromMap(Map<String, dynamic> map) =>
+      Project(id: map['id'] as String, name: map['name'] as String);
+
+  factory Project.fromDoc(DocumentSnapshot doc) {
+    final data = doc.data() as Map<String, dynamic>;
+    return Project(id: doc.id, name: data['name'] as String);
   }
-
-  factory Project.fromMap(Map<String, dynamic> map) {
-    return Project(
-      id: map['id'],
-      name: map['name'],
-    );
-  }
-
-  String toJson() => json.encode(toMap());
-  factory Project.fromJson(String source) =>
-      Project.fromMap(json.decode(source));
 }

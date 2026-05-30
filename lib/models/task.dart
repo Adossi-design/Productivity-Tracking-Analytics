3e6abac7a8317e3f53143175e678439e429cc79d
Adossi Fred William <f.adossi@alustudent.com>
2026-05-30 22:13:06 +0200

    Models a task and its parent-project link

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -8,21 +8,24 @@ class Task {
 
   Task({required this.id, required this.name, required this.projectId});
 
-  Map<String, dynamic> toMap() =>
-      {'id': id, 'name': name, 'projectId': projectId};
+  Map<String, dynamic> toMap() => {
+    'id': id,
+    'name': name,
+    'projectId': projectId,
+  };
 
   factory Task.fromMap(Map<String, dynamic> map) => Task(
-        id: map['id'] as String,
-        name: map['name'] as String,
-        projectId: map['projectId'] as String,
-      );
+    id: map['id'] as String,
+    name: map['name'] as String,
+    projectId: map['projectId'] as String,
+  );
 
   factory Task.fromDoc(DocumentSnapshot doc) {
-    final data = doc.data() as Map<String, dynamic>;
+    final data = (doc.data() as Map<String, dynamic>?) ?? const {};
     return Task(
       id: doc.id,
-      name: data['name'] as String,
-      projectId: data['projectId'] as String,
+      name: (data['name'] as String?) ?? 'Untitled',
+      projectId: (data['projectId'] as String?) ?? '',
     );
   }
 }
